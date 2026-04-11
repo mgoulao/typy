c3a7a2fa9fbdaed018b47f8dfba05a9df3017263
Manuel Goulão <msilvagoulao@gmail.com>
2026-04-11 13:20:13 +0100

    update publish workflows

---
+++ tests/unit/test_package_metadata.py
@@ -0,0 +1,7 @@
+from importlib.metadata import version
+
+import typy
+
+
+def test_package_version_matches_distribution_metadata():
+	assert typy.__version__ == version("typy")
--- typy/__init__.py
+++ typy/__init__.py
@@ -1,6 +1,23 @@
+from importlib.metadata import PackageNotFoundError, version
+
 import typy.builder as builder  # noqa: F401
 import typy.content as content  # noqa: F401
 import typy.encodable as encodable  # noqa: F401
 import typy.functions as functions  # noqa: F401
 import typy.markup as markup  # noqa: F401
 import typy.typst_encoder as typst_encoder  # noqa: F401
+
+try:
+	__version__ = version("typy")
+except PackageNotFoundError:
+	__version__ = "0+unknown"
+
+__all__ = [
+	"__version__",
+	"builder",
+	"content",
+	"encodable",
+	"functions",
+	"markup",
+	"typst_encoder",
+]
